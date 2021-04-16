78b04b5f78124b8ef89e8705a19d529a2ac550ff
snz0901@gmail.com <ngbt0409>
2021-04-16 21:47:41 +0900

    UNTIL: case-2

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 $(function(){
   // Offset the gap after the AOS.js emerging animation. 
-  // $( window ).on( "load", () => {
-  //   setTimeout(()=> {
+  $( window ).on( "load", () => {
+    setTimeout(()=> {
 
-  //   $("html, body").animate({scrollTop: 0}, 1000);
-  //   })
-  //   , 500})
+    // $("html, body").animate({scrollTop: 0}, 1000);
+    $(".top").css("overflow", "hidden")
+    }
+    , 1000) 
+  })
 })
